f8790559efc7ad2b9baad814ff65245b3b9eb748
Kanokpon Nagavajara <kanokpon26@gmail.com>
2025-07-12 09:45:32 +0700

    feat: add shadcn

--- frontend/src/layout/AppSideBar.tsx
+++ frontend/src/layout/AppSideBar.tsx
@@ -1,27 +1,45 @@
+import { Button } from "../components/ui/button";
+
 export default function AppSideBar() {
   return (
-    <div className="flex w-[250px] flex-col bg-inherit p-4">
-      <div className="space-y-2">
+    <div className="flex w-[250px] flex-col bg-inherit p-2">
+      <div className="space-y-6">
         <div className="flex items-center justify-between">
-          <button className="flex items-center">
+          <Button className="flex items-center">
             <img
               src="/logo.png"
               alt="Logo"
               className="border-brand-500 border-1 h-8 w-auto rounded-full"
             />
-          </button>
-          <button className="p-2">{`<`}</button>
+          </Button>
+          <Button className="rounded-md p-2 hover:bg-neutral-200">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="text-text-tertiary h-5 w-5"
+            >
+              <rect width="18" height="18" x="3" y="3" rx="2" />
+              <path d="M9 3v18" />
+            </svg>
+          </Button>
         </div>
 
-        <button className="w-full">New Chat</button>
-        <button className="w-full">Clear Chat History</button>
-        <button className="w-full">Library</button>
+        <Button className="w-full">New Chat</Button>
+        <Button className="w-full">Clear Chat History</Button>
+        <Button className="w-full">Library</Button>
       </div>
       <div className="space-y-2">
         {Array.from({ length: 5 }).map((_, i) => (
-          <button key={i} className="w-full">
+          <Button key={i} className="w-full">
             first_question
-          </button>
+          </Button>
         ))}
       </div>
     </div>
